8121970914042c3d1545a27633dcd17b0e972e41
Mauro Zegarra <mauro.zegarra@gmail.com>
2020-09-05 14:45:35 -0500

    improve shift in vertical

--- app/src/main/java/com/maurozegarra/example/marryme/MainActivity.kt
+++ app/src/main/java/com/maurozegarra/example/marryme/MainActivity.kt
@@ -12,8 +12,8 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var previousPositionY = 0f
-    private var previousPositionX = 0f
+    private var positionY = 0f
+    private var positionX = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,34 +37,81 @@ class MainActivity : AppCompatActivity() {
         val container = binding.buttonNo.parent as ViewGroup
         val containerW = container.width
         val containerH = container.height
-        var buttonWidth: Float = binding.buttonNo.width.toFloat()
+        val buttonWidth: Float = binding.buttonNo.width.toFloat()
         val buttonHeight: Float = binding.buttonNo.height.toFloat()
         val posX = binding.buttonNo.x
         val posY = binding.buttonNo.y
 
-        val spaceDownButton = containerH - (posY + buttonHeight * 2)
-        val spaceUpButton = posY - buttonHeight
+        moveVertical(containerH, posY, buttonHeight)
+        moveHorizontal(containerW, posX, buttonWidth)
+    }
 
-        val jumpUp = Random().nextBoolean()
+    private fun moveVertical(containerH: Int, posY: Float, buttonHeight: Float) {
+        val minimumFactor = 2
+        val minimumShift = buttonHeight * minimumFactor
+        val spaceAboveButton = posY - buttonHeight
+        val spaceBellowButton = containerH - (posY + buttonHeight * 2)
 
-        var shift = buttonHeight
+        val jumpTop = mayJumpTop(spaceAboveButton, spaceBellowButton, minimumShift)
+        val buttonsContained =
+            getButtonsContained(jumpTop, spaceAboveButton, spaceBellowButton, buttonHeight)
+        val remainingFactor = buttonsContained - minimumFactor
+        val shift = getShift(minimumShift, remainingFactor, buttonHeight)
 
-        if (jumpUp) {
-            shift = Math.random().toFloat() * spaceDownButton
-        } else {
-            shift = Math.random().toFloat() * -spaceUpButton
-        }
+        if (jumpTop)
+            positionY -= shift
+        else
+            positionY += shift
 
-        previousPositionY += shift
-
-        val moverVertical =
-            ObjectAnimator.ofFloat(binding.buttonNo, View.TRANSLATION_Y, previousPositionY)
+        val moverVertical = ObjectAnimator.ofFloat(binding.buttonNo, View.TRANSLATION_Y, positionY)
         moverVertical.interpolator = AccelerateInterpolator(1f)
         moverVertical.duration = 100
         moverVertical.start()
+    }
 
-        ////////////////////////////////////////////////////////////////////////////////////////////
+    private fun mayJumpTop(
+        spaceAboveButton: Float,
+        spaceBellowButton: Float,
+        minimumShift: Float
+    ): Boolean {
+        val couldJumpTop = spaceAboveButton >= minimumShift
+        val couldJumpBottom = spaceBellowButton >= minimumShift
+        val spaceAreEquals = spaceAboveButton == spaceBellowButton
 
+        if (couldJumpTop && couldJumpBottom) {
+            return if (spaceAreEquals)
+                true
+            else
+                Random().nextBoolean()
+        }
+
+        if (couldJumpTop) return true
+        if (couldJumpBottom) return false
+
+        // hasta aquí todas las posibilidades están agotadas, pongo true solo por preferir un salto
+        // hacia arriba pero, no debería pasar
+        return true
+    }
+
+    private fun getButtonsContained(
+        jumpTop: Boolean,
+        spaceAboveButton: Float,
+        spaceBellowButton: Float,
+        buttonHeight: Float
+    ): Int {
+        return if (jumpTop)
+            spaceAboveButton.toInt() / buttonHeight.toInt()
+        else
+            spaceBellowButton.toInt() / buttonHeight.toInt()
+    }
+
+    // Mínimo debe devolver 3 veces el alto del botón y máximo el número de veces contenido el botón
+    // en el espacio disponible
+    private fun getShift(minimumShift: Float, remainingFactor: Int, buttonHeight: Float): Float {
+        return minimumShift + Math.random().toFloat() * (remainingFactor * buttonHeight)
+    }
+
+    private fun moveHorizontal(containerW: Int, posX: Float, buttonWidth: Float) {
         val spaceRightButton = containerW - (posX + buttonWidth * 2)
         val spaceLeftButton = posX - buttonWidth
 
@@ -76,10 +123,10 @@ class MainActivity : AppCompatActivity() {
             Math.random().toFloat() * -spaceLeftButton
         }
 
-        previousPositionX += shiftHorizontal
+        positionX += shiftHorizontal
 
         val moverHorizontal =
-            ObjectAnimator.ofFloat(binding.buttonNo, View.TRANSLATION_X, previousPositionX)
+            ObjectAnimator.ofFloat(binding.buttonNo, View.TRANSLATION_X, positionX)
         moverHorizontal.interpolator = AccelerateInterpolator(1f)
         moverHorizontal.duration = 100
         moverHorizontal.start()
